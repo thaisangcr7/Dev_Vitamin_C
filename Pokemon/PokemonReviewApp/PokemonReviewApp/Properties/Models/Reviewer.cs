e7008b416cbcef0b585aab40c83db6fc2511e4e7
Sang Thai <thaisangcr7@gmail.com>
2024-10-11 12:26:39 -0400

    create models

---
+++ Pokemon/PokemonReviewApp/PokemonReviewApp/Properties/Models/Reviewer.cs
@@ -0,0 +1,9 @@
+﻿namespace PokemonReviewApp.Properties.Models
+{
+    public class Reviewer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
