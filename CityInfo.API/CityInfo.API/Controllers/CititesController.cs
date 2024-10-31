6b4a606a4d790da920377fccb25479b4e8ffc6cf
Sang Thai <thaisangcr7@gmail.com>
2024-10-31 00:48:45 -0400

    get exception because of missing attribute route

---
+++ CityInfo.API/CityInfo.API/Controllers/CititesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    public class CititesController : ControllerBase
+    // Let have the CititesController class derives from the ControllerBase
+    {
+        // json result class: this result a jsonifies version of whatever we passed into the cosntuctor of jsonresult
+        public JsonResult GetCities()
+        {
+            // because we dont have the model class yet,so we gona create objects
+            // id and a name
+
+            return new JsonResult(
+                
+                new List<object> 
+                {
+                    new {id = 1, Name = "New York City" },
+                    new {id = 2, Name = "Antwerp" }
+                }
+               
+                );
+
+        }
+
+    }
+}
