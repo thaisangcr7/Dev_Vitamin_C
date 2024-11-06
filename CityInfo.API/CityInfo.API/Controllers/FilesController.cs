4515798de89b02c34f3d3d6ffcc0d04998f1588d
Sang Thai <thaisangcr7@gmail.com>
2024-11-06 00:09:09 -0500

    Getting file - adding Files Controller!

---
+++ CityInfo.API/CityInfo.API/Controllers/FilesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+
+        private readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider;
+
+        public FilesController(
+            FileExtensionContentTypeProvider fileExtensionContentTypeProvider)
+        {
+            _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider
+                ?? throw new System.ArgumentNullException(
+                    nameof(fileExtensionContentTypeProvider));
+        }
+        
+        
+        
+        [HttpGet("{fileId}")]
+
+        public ActionResult GetFile(string fileId)
+        {
+            // Look up the actual file, depending on the fileId
+            var pathToFile = "2023. J.Resume Sang Thai.pdf";
+
+            // Check whether the file exists
+            if (!System.IO.File.Exists(pathToFile))
+            {
+                return NotFound();
+            }
+
+            if (!_fileExtensionContentTypeProvider.TryGetContentType(
+                pathToFile, out var contentType))
+            {
+                contentType = "application/octet-stream";
+
+            }
+
+
+            var bytes = System.IO.File.ReadAllBytes(pathToFile);
+            return File(bytes, contentType, Path.GetFileName(pathToFile));
+        }
+
+    }
+}
--- CityInfo.API/CityInfo.API/Program.cs
+++ CityInfo.API/CityInfo.API/Program.cs
@@ -1,23 +1,31 @@
+using Microsoft.AspNetCore.StaticFiles;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
-// Adding Problem details
-builder.Services.AddProblemDetails(options =>
+builder.Services.AddControllers( options => 
 {
-    options.CustomizeProblemDetails = ctx =>
-    {
-        ctx.ProblemDetails.Extensions.Add("additionInfo", "Additional info example ");
+    options.ReturnHttpNotAcceptable = true;
+}).AddXmlDataContractSerializerFormatters();
 
-        ctx.ProblemDetails.Extensions.Add("sever", Environment.MachineName);
-    };
-});
+//// Adding Problem details - Manipulate the error response
+//builder.Services.AddProblemDetails(options =>
+//{
+//    options.CustomizeProblemDetails = ctx =>
+//    {
+//        ctx.ProblemDetails.Extensions.Add("additionInfo", "Additional info example ");
+
+//        ctx.ProblemDetails.Extensions.Add("sever", Environment.MachineName);
+//    };
+//});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
 
 var app = builder.Build();
 
