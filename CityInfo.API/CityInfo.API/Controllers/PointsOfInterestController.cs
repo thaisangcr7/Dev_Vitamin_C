6f5f39da441268c32925fe91ad030a78b49dc375
Sang Thai <thaisangcr7@gmail.com>
2024-11-10 01:06:47 -0500

    A full update

--- CityInfo.API/CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -49,7 +49,8 @@ namespace CityInfo.API.Controllers
 
             return Ok(pointOfInterest);
         }
-
+        
+        // Create a new resource
         [HttpPost]
         public ActionResult<PointOfInterestDto> CreatePointOfInterest(
             int cityId,
@@ -80,9 +81,39 @@ namespace CityInfo.API.Controllers
                     cityId = cityId,
                     pointOfInterestId = finalPointOfInterest.Id
                 },
-                finalPointOfInterest);
-                
+                finalPointOfInterest);   
             }
+
+        // Full update a resource
+        [HttpPut("{pointofinterestid}")]
+        public ActionResult UpdatePointOfInterest(int cityId, int pointOfInterestId,
+            PointOfInterestForUpdateDto pointOfInterest)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if(city == null)
+            {
+                return NotFound();
+            }
+            //Find point of Interest
+            var pointOfInterestFromStore = city.PointsOfInterest
+                .FirstOrDefault(c => c.Id == pointOfInterestId);
+
+            if (pointOfInterestFromStore == null)
+            {
+                return NotFound();
+            }
+
+            pointOfInterestFromStore.Name = pointOfInterest.Name;
+            pointOfInterestFromStore.Description = pointOfInterest.Description;
+
+            return NoContent();
+
+
+        }
+
+        
+           
+ 
 
     }
 }
