d0ad88f8cb6c555d17a2e81c57a70d96f962b35b
Sang Thai <thaisangcr7@gmail.com>
2024-11-15 15:33:37 -0500

    Install thirdparty seriLog and configure serilog and use serilog instead of the built in logger

--- CityInfo.API/CityInfo.API/Program.cs
+++ CityInfo.API/CityInfo.API/Program.cs
@@ -1,4 +1,12 @@
 using Microsoft.AspNetCore.StaticFiles;
+using Serilog;
+
+//Configure Serilog
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("logs/cityinfo.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Logging.ClearProviders();
