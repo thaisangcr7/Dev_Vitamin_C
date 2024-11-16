767d5febce391489dbbfb308135bb176d13789a8
Sang Thai <thaisangcr7@gmail.com>
2024-11-16 18:22:42 -0500

    Configuration Files and Scoping Configuration to Environments

--- CityInfo.API/CityInfo.API/Services/CloudMailService.cs
+++ CityInfo.API/CityInfo.API/Services/CloudMailService.cs
@@ -2,10 +2,16 @@
 {
     public class CloudMailService : IMailService
     {
-        private string _mailTo = "admin@mycompany.com";
-        private string _mailFrom = "noreply@company.com";
+        private string _mailTo = string.Empty;
+        private string _mailFrom = string.Empty;
 
         // add a method to this class to sending a mail
+        public CloudMailService(IConfiguration configuration)
+        {
+            _mailTo = configuration["mailSettings:mailToAddress"];
+            _mailFrom = configuration["mailSettings:mailFromAddress"];
+        }
+
         public void Send(string subject, string message)
         {
             //send mail_ output to Console Window
--- CityInfo.API/CityInfo.API/Services/LocalMailService.cs
+++ CityInfo.API/CityInfo.API/Services/LocalMailService.cs
@@ -2,8 +2,16 @@
 {
     public class LocalMailService : IMailService
     {
-        private string _mailTo = "admin@mycompany.com";
-        private string _mailFrom = "noreply@company.com";
+        private string _mailTo = string.Empty;
+        private string _mailFrom = string.Empty;
+
+        // using a constructor to inject it into our LocalMailService 
+        public LocalMailService(IConfiguration configuration)
+        {
+            _mailTo = configuration["mailSettings:mailToAddress"];
+            _mailFrom = configuration["mailSettings:mailFromAddress"];
+
+        }
 
         // add a method to this class to sending a mail
         public void Send(string subject, string message)
