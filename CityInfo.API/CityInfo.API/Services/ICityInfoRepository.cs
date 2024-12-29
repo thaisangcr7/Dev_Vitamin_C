c3cddb89a8a94228b6cadd338108ce7c97f8ccba
sangth <thaisangcr7@gmail.com>
2024-12-28 22:31:29 -0500

    Using Information from the Token in your Controller

--- CityInfo.API/CityInfo.API/Services/ICityInfoRepository.cs
+++ CityInfo.API/CityInfo.API/Services/ICityInfoRepository.cs
@@ -24,6 +24,7 @@ namespace CityInfo.API.Services
 
         void DeletePointOfInterest(PointOfInterest pointOfInterest);
 
+        Task<bool> CityNameMatchesCityId(string? cityName, int cityId);
         Task<bool> SaveChangesAsync();
     }
 }
